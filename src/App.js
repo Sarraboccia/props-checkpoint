42de69924053bba4d5e3734c4330111c6084f646
Sara Boccia <saraboccia@Saras-MacBook-Air.local>
2021-10-29 10:22:58 +0100

    checkpoint-props

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+// import logo from './logo.svg';
+import "./App.css";
+import Profile from "./Profile/Profile.js";
+import Memories from "./Memories.JPG"
+
+function App() {
+
+  function showAlert (name) {
+      
+  return alert(name)
+     
+  }
+
+
+  return (
+    <div className="App">
+      <Profile
+        fullName="Sara Boccia"
+        bio="If you get tired, learn to rest, not to quit."
+        profession="Web development"
+        showAlert={showAlert}
+      >
+        <div>
+        <img src={Memories} alt="children props" width= '250px' />
+        <h1>DOUZ</h1>
+   </div>
+   
+      </Profile>
+    </div>
+    );
+   
+  
+}
+
+export default App;
